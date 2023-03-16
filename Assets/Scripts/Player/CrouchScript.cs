2c10e8092e7665b1272de67c6899316748dca49e
Daadaa Nabil <103863031+RyzKum@users.noreply.github.com>
2023-03-16 11:50:43 +0100

    Progress

--- Assets/Scripts/Player/CrouchScript.cs
+++ Assets/Scripts/Player/CrouchScript.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class CrouchScript : MonoBehaviour
 {
+    public PlayerMovement player;
+
     public float crouchHeight;
     private float normalHeight;
-    private float yInput;
 
+    public Transform headCheck;
+    public float headCheckLength;
+    public LayerMask groundMask;
+
+    private float yInput;
 
     private void Start()
     {
@@ -18,16 +24,35 @@ public class CrouchScript : MonoBehaviour
     {
         yInput = Input.GetKey(KeyCode.C) ? -1 : 0;
 
-        if (yInput < 0)
+        bool isHeadHitting = HeadDetect();
+
+        if (yInput < 0 || isHeadHitting && player.IsGrounded())
         {
-            if(transform.localScale.y != crouchHeight)
-            transform.localScale=new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
+            if (transform.localScale.y != crouchHeight)
+                transform.localScale = new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
         }
         else
         {
-            if(transform.localScale.y != normalHeight)
-            transform.localScale=new Vector3(transform.localScale.x, normalHeight, transform.localScale.z);
-
+            if (transform.localScale.y != normalHeight)
+                transform.localScale = new Vector3(transform.localScale.x, normalHeight, transform.localScale.z);
         }
     }
+
+    bool HeadDetect()
+    {
+        bool hit = Physics2D.Raycast(headCheck.position, Vector2.up, headCheckLength, groundMask);
+        return hit;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (headCheck == null) return;
+
+        Vector2 from = headCheck.position;
+        Vector2 to = new Vector2(headCheck.position.x, headCheck.position.y + headCheckLength);
+
+        Gizmos.DrawLine(from, to);
+    }
+    
+    
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,9 +11,9 @@ public class PlayerMovement : MonoBehaviour
 
     private bool canDash = true;
     private bool isDashing;
-    private float dashingPower = 24f;
+    private float dashingPower = 60f;
     private float dashingTime = 0.2f;
-    private float dashingCooldown = 1f;
+    private float dashingCooldown = 0.2f;
 
     public float KBForce;
     public float KBCounter;
@@ -103,7 +103,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private bool IsGrounded()
+    public bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
